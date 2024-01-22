9ef58c6f7a8acfd548fcf147ad10dff9e400581d
Filipe Lira De Oliveira <filipeliradeoliveira05@gmail.com>
2024-01-22 17:58:30 -0300

    Adicionando o projeto completo

---
+++ script.js
@@ -0,0 +1,32 @@
+const divimg = document.querySelector("#imagens");
+const url = 'https://nekos.best/api/v2/husbando';
+const form = document.querySelector("#formulario");
+const btn = document.querySelector("#gerar");
+let divinput=document.querySelector("#divinput");
+const quantidadeinput = document.querySelector("#quantidade");
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const quantidade = quantidadeinput.value;
+    console.log(quantidade);
+    quantidadeinput.value = "";
+    divimg.removeChild(divinput);
+    exibiranimes(url, quantidade);
+
+});
+async function exibiranimes(url, quantidade) {
+    for (i = 1; i <=quantidade; i++) {
+        fetch(url)
+        .then(res => res.json())
+            .then(dados => {
+                console.log(dados);
+                dados.results.forEach(elemento => {
+                    let anime = elemento.url;
+                    let novaImagem = document.createElement('img');
+                    novaImagem.src = anime;
+                    novaImagem.classList.add('imganime');
+                    divimg.appendChild(novaImagem);
+
+                })
+            })
+    }
+}
